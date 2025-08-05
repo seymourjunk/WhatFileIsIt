be4ff261e26cd3c83eda66124899f3248372b135
seymourjunk <gololobova.galya@gmail.com>
2025-08-05 17:45:48 +0300

    add convert epoch timestamp function

--- utils.cpp
+++ utils.cpp
@@ -15,3 +15,11 @@ void print_hex(const uint8_t* arr, uint32_t num)
         printf("%02x", arr[i]);
     }
 }
+
+void convert_epoch_timestamp(char* str_ptr, size_t size, uint32_t timestamp)
+{ 
+    time_t epoch_time = timestamp;
+    tm *time_info = localtime(&epoch_time);
+
+    strftime(str_ptr, size, "%a %d-%m-%Y %H:%M:%S", time_info);
+}
--- utils.h
+++ utils.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void print_binary(const int32_t num);
 void print_hex(const uint8_t* arr, uint32_t num);
+void convert_epoch_timestamp(char* str_ptr, size_t size, uint32_t timestamp);
